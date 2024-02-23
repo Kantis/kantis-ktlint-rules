acd532146a02333bfb544f88500c84e5fe9c9f85
Emil Kantis <emil@codeboss.se>
2024-02-23 07:53:58 +0100

    Added RuleSetProvider

---
+++ src/main/kotlin/com/github/kantis/ktlint/KantisRulesetProvider.kt
@@ -0,0 +1,12 @@
+package com.github.kantis.ktlint
+
+import com.pinterest.ktlint.cli.ruleset.core.api.RuleSetProviderV3
+import com.pinterest.ktlint.rule.engine.core.api.RuleProvider
+
+public class KantisRulesetProvider : RuleSetProviderV3(rulesetId) {
+   override fun getRuleProviders(): Set<RuleProvider> = setOf(
+      RuleProvider { SingleLambdaArgumentRule() },
+      RuleProvider { AdjustedTrailingCommaOnCallSiteRule() },
+      RuleProvider { ValueInAssignmentStartsOnSameLineRule() },
+   )
+}
